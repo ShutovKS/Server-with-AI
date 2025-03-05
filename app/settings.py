d1b20b42a3e79672708cc12845e02243fa097140
ShutovKS <schutow.kir@yandex.ru>
2025-03-05 11:00:16 +0300

    Add settings llm

---
+++ app/settings.py
@@ -0,0 +1,68 @@
+﻿import logging
+import os
+
+from llama_index.core.settings import Settings
+
+logger = logging.getLogger("uvicorn")
+
+
+def init_settings():
+    model_provider = os.getenv("LLM_PROVIDER", "huggingface")
+    logger.info(f"Initializing settings with model provider: {model_provider}")
+
+    match model_provider:
+        case "huggingface":
+            _init_huggingface()
+        case "lm-studio":
+            _init_lm_studio()
+
+    logger.info("Model provider initialized")
+
+    _init_embed_model()
+
+    Settings.chunk_size = int(os.getenv("CHUNK_SIZE", "1024"))
+    Settings.chunk_overlap = int(os.getenv("CHUNK_OVERLAP", "20"))
+
+
+def _init_huggingface():
+    from llama_index.llms.huggingface import HuggingFaceLLM
+
+    logger.info("Initializing HuggingFace LLM")
+
+    Settings.llm = HuggingFaceLLM(
+        model_name=os.getenv("LLM_MODEL"),
+        tokenizer_name=os.getenv("LLM_MODEL"),
+        max_new_tokens=int(os.getenv("LLM_MAX_NEW_TOKENS", "256")),
+    )
+
+
+def _init_lm_studio():
+    from llama_index.llms.lmstudio import LMStudio
+
+    logger.info("Initializing LM Studio LLM")
+
+    Settings.llm = LMStudio(
+        model_name=os.getenv("LLM_MODEL"),
+        base_url=os.getenv("LM_STUDIO_BASE_URL", "http://127.0.0.1:1234"),
+        temperature=0.7,
+        is_chat_model=False,
+        request_timeout=180,
+        timeout=180,
+    )
+
+
+def _init_embed_model():
+    logger.info("Initializing embed model")
+
+    from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+    Settings.embed_model = HuggingFaceEmbedding(
+        model_name=os.getenv("EMBEDDING_MODEL", "all-MiniLM-L6-v2"),
+        trust_remote_code=os.getenv("EMBEDDING_TRUST_REMOTE_CODE", "false").lower() == "true",
+        backend=os.getenv("EMBEDDING_BACKEND", "torch"),
+    )
+
+    logger.info("Embed model initialized")
+
+    Settings.chunk_size = int(os.getenv("CHUNK_SIZE", "1024"))
+    Settings.chunk_overlap = int(os.getenv("CHUNK_OVERLAP", "20"))
