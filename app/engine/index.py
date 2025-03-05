f2179328fe0e02cbf929e606dabc7c199ddd25ea
ShutovKS <schutow.kir@yandex.ru>
2025-03-05 10:59:38 +0300

    Add files -> index

---
+++ app/engine/index.py
@@ -0,0 +1,33 @@
+﻿import logging
+from typing import Optional
+
+from llama_index.core import VectorStoreIndex
+from llama_index.core.callbacks import CallbackManager
+from pydantic import BaseModel, Field
+
+from app.engine.vectordb import get_vector_store
+
+logger = logging.getLogger("uvicorn")
+
+
+class IndexConfig(BaseModel):
+    callback_manager: Optional[CallbackManager] = Field(
+        default=None,
+    )
+
+
+def get_index(config: IndexConfig = None):
+    if config is None:
+        config = IndexConfig()
+
+    logger.info("Connecting vector store...")
+
+    store = get_vector_store()
+    index = VectorStoreIndex.from_vector_store(
+        store,
+        callback_manager=config.callback_manager,
+    )
+
+    logger.info("Finished load index from vector store.")
+
+    return index
