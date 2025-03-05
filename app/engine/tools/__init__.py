17b3b96587b701394c23c49fc60af811a7e6c260
ShutovKS <schutow.kir@yandex.ru>
2025-03-05 11:02:05 +0300

    Add wikipedia tool

---
+++ app/engine/tools/__init__.py
@@ -0,0 +1,68 @@
+import importlib
+import os
+from typing import Dict, List, Union
+
+import yaml
+from llama_index.core.tools.function_tool import FunctionTool
+from llama_index.core.tools.tool_spec.base import BaseToolSpec
+
+
+class ToolFactory:
+    @staticmethod
+    def load_tools(tool_name: str, config: dict) -> List[FunctionTool]:
+        source_package = "app.engine.tools"
+        try:
+            if "ToolSpec" in tool_name:
+                tool_package, tool_cls_name = tool_name.split(".")
+                module_name = f"{source_package}.{tool_package}"
+                module = importlib.import_module(module_name)
+                tool_class = getattr(module, tool_cls_name)
+                tool_spec: BaseToolSpec = tool_class(**config)
+                return tool_spec.to_tool_list()
+            else:
+                module = importlib.import_module(f"{source_package}.{tool_name}")
+                tools = module.get_tools(**config)
+                if not all(isinstance(tool, FunctionTool) for tool in tools):
+                    raise ValueError(
+                        f"The module {module} does not contain valid tools"
+                    )
+                return tools
+        except ImportError as e:
+            raise ValueError(f"Failed to import tool {tool_name}: {e}")
+        except AttributeError as e:
+            raise ValueError(f"Failed to load tool {tool_name}: {e}")
+
+    @staticmethod
+    def from_env(
+            map_result: bool = False,
+    ) -> Union[Dict[str, List[FunctionTool]], List[FunctionTool]]:
+        """
+        Load tools from the configured file.
+
+        Args:
+            map_result: If True, return a map of tool names to their corresponding tools.
+
+        Returns:
+            A dictionary of tool names to lists of FunctionTools if map_result is True,
+            otherwise a list of FunctionTools.
+        """
+        tools: Union[Dict[str, FunctionTool], List[FunctionTool]] = (
+            {} if map_result else []
+        )
+
+        if os.path.exists("config/tools.yaml"):
+            with open("config/tools.yaml", "r") as f:
+                tool_configs = yaml.safe_load(f)
+                for tool_name, config in tool_configs.items():
+                    loaded_tools = ToolFactory.load_tools(
+                        tool_name,
+                        config,
+                    )
+                    if map_result:
+                        tools.update(  # type: ignore
+                            {tool.metadata.name: tool for tool in loaded_tools}
+                        )
+                    else:
+                        tools.extend(loaded_tools)  # type: ignore
+
+        return tools
---
+++ app/engine/tools/wikipedia_tool.py
@@ -0,0 +1,54 @@
+﻿from typing import Any, Dict
+
+from llama_index.core.tools import FunctionTool
+
+
+def load_data(
+        page: str, lang: str = "ru", **load_kwargs: Dict[str, Any]
+) -> str:
+    """
+    Retrieve a Wikipedia page. Useful for learning about a particular concept that isn't private information.
+
+    Args:
+        page (str): Title of the page to read.
+        lang (str): Language of Wikipedia to read.
+    """
+    import wikipedia
+
+    wikipedia.set_lang(lang)
+    try:
+        wikipedia_page = wikipedia.page(page, **load_kwargs, auto_suggest=False)
+    except wikipedia.PageError:
+        return "Unable to load page. Try searching instead."
+    return wikipedia_page.content
+
+
+def search_data(query: str, lang: str = "ru") -> str:
+    """
+    Search Wikipedia for a page related to the given query.
+    Use this tool when `load_data` returns no results.
+
+    Args:
+        query (str): the string to search for
+    """
+    import wikipedia
+
+    pages = wikipedia.search(query)
+    if len(pages) == 0:
+        return "No search results."
+    return load_data(pages[0], lang)
+
+
+def get_tools(**kwargs):
+    return [
+        FunctionTool.from_defaults(
+            fn=load_data,
+            name="Wikipedia Page",
+            description="Retrieve a Wikipedia page.",
+        ),
+        FunctionTool.from_defaults(
+            fn=search_data,
+            name="Wikipedia Search",
+            description="Search Wikipedia for a page related to the given query.",
+        ),
+    ]
