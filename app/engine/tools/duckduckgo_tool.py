160d580059593ee91e516d199f6c1684db30ccd8
ShutovKS <schutow.kir@yandex.ru>
2025-03-05 11:02:20 +0300

    Add duckduckgo search tool

---
+++ app/engine/tools/duckduckgo_tool.py
@@ -0,0 +1,42 @@
+from llama_index.core.tools.function_tool import FunctionTool
+
+
+def duckduckgo_search(
+        query: str,
+        region: str = "wt-wt",
+        max_results: int = 10,
+):
+    """
+    Use this function to search for any query in DuckDuckGo.
+    Args:
+        query (str): The query to search in DuckDuckGo.
+        region Optional(str): The region to be used for the search in [country-language] convention, ex us-en, uk-en, ru-ru, etc...
+        max_results Optional(int): The maximum number of results to be returned. Default is 10.
+    """
+    try:
+        from duckduckgo_search import DDGS
+    except ImportError:
+        raise ImportError(
+            "duckduckgo_search package is required to use this function."
+            "Please install it by running: `poetry add duckduckgo_search` or `pip install duckduckgo_search`"
+        )
+
+    with DDGS() as ddg:
+        results = list(
+            ddg.text(
+                keywords=query,
+                region=region,
+                max_results=max_results,
+            )
+        )
+    return results
+
+
+def get_tools(**kwargs):
+    return [
+        FunctionTool.from_defaults(
+            fn=duckduckgo_search,
+            name="DuckDuckGo Search",
+            description="Search for any query in DuckDuckGo search engine.",
+        ),
+    ]
